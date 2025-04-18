fc5e41f84a32c931aa19217f22e91c663e1d9b50
therealchevydean <ewingjosh817@gmail.com>
2025-04-18 06:24:42 -0700

    Delete card.js

--- card.js
+++
@@ -1,7 +0,0 @@
-export function Card({ children }) {
-  return <div className="border rounded-2xl shadow p-4 bg-white">{children}</div>;
-}
-
-export function CardContent({ children, className }) {
-  return <div className={className}>{children}</div>;
-}
