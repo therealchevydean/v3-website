441efe984e9b01af841c503fbfde56f2c018a0f7
therealchevydean <ewingjosh817@gmail.com>
2025-08-15 02:52:05 -0700

    chore: keep JS homepage; remove duplicate; set mixed JS/TS tsconfig; add .gitignore

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-﻿export default function V3Hub() {
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial"  }}>
-      <h1>V3 Hub</h1>
-      <p>This is the V3 Hub page placeholder.</p>
-    </div>
-  );
-}
